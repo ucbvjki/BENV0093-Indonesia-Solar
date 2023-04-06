d5d5113dd4ade4e0e8659297258e2ed41f5520be
ucbvjki <ucbvjki@ucl.ac.uk>
2023-04-06 14:21:56 +0100

    changed dataset

--- Map - Installed.R
+++ Map - Installed.R
@@ -9,3 +9,11 @@ library(RColorBrewer)
 
 # Import Files ------------------------------------------------------------
 plant <- read_csv(url("https://raw.githubusercontent.com/ucbvjki/BENV0093-Indonesia-Solar/main/Data/Plant%20Data.csv"))
+boundary0 <- read_sf(dsn = "Data/Shapefiles/idn_admbnda_adm0_bps_20200401.shp", layer = "SHAPEFILE")
+  
+  
+boundary1 <-
+
+
+# Clean Data --------------------------------------------------------------
+##Create coordinates
